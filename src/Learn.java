aacf96f43eef805cef3f1aad0d7febd41b0ad893
Vinaya <Vinaya@Vinaya-PC>
2017-07-26 01:06:33 +0530

    test

---
+++ src/Learn.java
@@ -0,0 +1,14 @@
+
+public class Learn {
+
+	
+	/*	WebDriver driver = new FirefoxDriver();
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	System.out.print("Enter the input to find the element using locator");
+	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	String str = input.readLine();
+	String ab = "animal";
+	String ab = "Animal";
+	ab.lastIndexOf(ch);*/
+}
